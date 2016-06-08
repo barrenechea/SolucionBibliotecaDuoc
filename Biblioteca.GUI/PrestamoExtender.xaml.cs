1207b6ddd1449a3b6e248fbfa0cd8dbfe147e064
dominniquenique <dominiqueniqueee@gmail.com>
2016-06-08 13:43:43 -0400

    error de nombre en la vista arreglado

---
+++ Biblioteca.GUI/PrestamoExtender.xaml.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MahApps.Metro.Controls;
+using MahApps.Metro.Controls.Dialogs;
+
+namespace Biblioteca.GUI
+{
+    /// <summary>
+    /// Interaction logic for PrestamoExtender.xaml
+    /// </summary>
+    public partial class PrestamoExtender : MetroWindow
+    {
+        private string _nroFicha;
+        public PrestamoExtender()
+        {
+            InitializeComponent();
+        }
+
+        private async void SearchPrestamoDialog()
+        {
+            var settings = new MetroDialogSettings
+            {
+                AffirmativeButtonText = "Buscar",
+                NegativeButtonText = "Cancelar",
+                ColorScheme = MetroDialogOptions.ColorScheme
+            };
+
+            var nroFicha = await this.ShowInputAsync("Buscar", "Ingrese el número de ficha del usuario", settings);
+
+            if (string.IsNullOrWhiteSpace(nroFicha))
+            {
+                new PanelAdmin().Show();
+                Close();
+                return;
+            }
+
+            var exists = App.Users.FetchUsuario(nroFicha);
+            if (exists.Status)
+            {
+                _nroFicha = nroFicha;
+                LoadData();
+            }
+            else
+            {
+                new PanelAdmin(exists).Show();
+                Close();
+            }
+        }
+
+        private void LoadData()
+        {
+            
+        }
+
+        private void BtnLogout_OnClick(object sender, RoutedEventArgs e)
+        {
+            App.Login.Logout();
+            new Inicio().Show();
+            Close();
+        }
+
+        private void BtnVolver_Click(object sender, RoutedEventArgs e)
+        {
+            new Inicio().Show();
+            Close();
+        }
+        private void WindowHasLoaded(object sender, RoutedEventArgs e)
+        {
+            SearchPrestamoDialog();
+        }
+
+        private void BtnExtender_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+    }
+}
