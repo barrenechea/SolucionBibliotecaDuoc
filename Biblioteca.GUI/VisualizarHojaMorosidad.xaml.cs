a13015592d969ab0ddbbaa0edff6cc1c839e1178
dominniquenique <dominiqueniqueee@gmail.com>
2016-06-13 18:29:59 -0400

    vista hoja de morosidad

---
+++ Biblioteca.GUI/VisualizarHojaMorosidad.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MahApps.Metro.Controls;
+
+namespace Biblioteca.GUI
+{
+    /// <summary>
+    /// Interaction logic for VisualizarHojaMorosidad.xaml
+    /// </summary>
+    public partial class VisualizarHojaMorosidad : MetroWindow
+    {
+        public VisualizarHojaMorosidad()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Event that loads when user clicks on the Logout button
+        /// </summary>
+        /// <param name="sender">The object that triggered this event</param>
+        /// <param name="e">Parameters (optional)</param>
+        private void BtnLogout_OnClick(object sender, RoutedEventArgs e)
+        {
+            App.Users.ClearPersistantData();
+            App.Admins.Logout();
+            new Inicio().Show();
+            Close();
+        }
+
+        /// <summary>
+        /// Event that loads when user clicks on the Back button
+        /// </summary>
+        /// <param name="sender">The object that triggered this event</param>
+        /// <param name="e">Parameters (optional)</param>
+        private void BtnBack_Click(object sender, RoutedEventArgs e)
+        {
+            App.Users.ClearPersistantData();
+            new PanelAdmin().Show();
+            Close();
+        }
+    }
+}
