fb5d7cbc8955c3d67116fdfaa4937388722d0c5b
SebastianBar <sbarrenechea@live.com>
2016-06-16 20:25:56 -0400

    GENERAL - Reference optimizations

--- Biblioteca.GUI/Properties/Resources.Designer.cs
+++ Biblioteca.GUI/Properties/Resources.Designer.cs
@@ -44,7 +44,7 @@ namespace Biblioteca.GUI.Properties
             {
                 if ((resourceMan == null))
                 {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Biblioteca.GUI.Properties.Resources", typeof(Resources).Assembly);
+                    var temp = new global::System.Resources.ResourceManager("Biblioteca.GUI.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
